731d58091a16f0d2af2df2a581dbd167c9545acb
Astro <astro@spaceboyz.net>
2011-06-18 17:07:25 +0200

    bin/magicsig: use new interface

--- bin/magicsig.js
+++ bin/magicsig.js
@@ -1,24 +1,17 @@
 #!/usr/bin/env node
 
 var Util = require('util');
-var Bn = require('bignumber');
-var Salmon = require('ostatus').salmon;
-
-var bits = 2048;
-var exponent = 'deadbeef';
+var Salmon = require('../lib/ostatus').salmon;
 
 
-var key = new Bn.Key();
 Util.puts("Generating key... this can take a while due to pure-JSness");
-key.generate(bits, exponent);
+var keys = Salmon.generateKeys();
 
 // Public
 Util.puts("Public key:");
-Util.puts("data:application/magic-public-key,RSA." +
-	  Salmon.base64url_encode(key.n.toByteArray()) +
-	  "." +
-	  Salmon.base64url_encode(new Bn.BigInteger(exponent, 16).toByteArray()));
+Util.puts("data:application/magic-public-key," + keys.public);
+Util.puts("(Put it in your user's LRDD file)");
 
 // Private
 Util.puts("Private key:");
-Util.puts(Salmon.base64url_encode(key.d.toByteArray()));
+Util.puts(keys.private);
